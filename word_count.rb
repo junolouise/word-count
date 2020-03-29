34d9af4818e0c98252aa58948b12440c6a80d006
Katie Rodgers <katierodgers95@gmail.com>
2020-03-29 14:14:17 +0100

    counts one word

--- word_count.rb
+++ word_count.rb
@@ -6,3 +6,14 @@ To get started with TDD, see the `README.md` file in your
 `ruby/word-count` directory.
 =end
 
+class Phrase
+
+  def initialize(word)
+    @word = word
+  end
+
+  def word_count
+    seperate_word = @word.split
+    result = seperate_word.each_with_object(Hash.new(0)) { |word, counts| counts[word] += 1 }
+  end
+end
