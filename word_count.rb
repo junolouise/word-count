efbcec7c9a8dc15b78905b3d70f9daae9677700e
Katie Rodgers <katierodgers95@gmail.com>
2020-03-29 17:03:17 +0100

    allows for commas

--- word_count.rb
+++ word_count.rb
@@ -9,7 +9,7 @@ require 'pry'
 class Phrase
 
   def initialize(word)
-    @word = word.downcase.gsub(/[^a-z\s0-9]/, ' ')
+    @word = word.downcase.gsub(/[^a-z\s0-9']/, ' ')
   end
 
   def word_count
